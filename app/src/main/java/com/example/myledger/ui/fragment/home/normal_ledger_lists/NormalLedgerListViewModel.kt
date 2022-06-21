28ce2e63d6e2c377d0c62940ffeffc0b7a2cd816
Theeranat Sreecharoen <theeranatsre@gmail.com>
2022-06-21 18:48:59 +0700

    featrue : add home tap layout

---
+++ app/src/main/java/com/example/myledger/ui/fragment/home/normal_ledger_lists/NormalLedgerListViewModel.kt
@@ -0,0 +1,7 @@
+package com.example.myledger.ui.fragment.home.normal_ledger_lists
+
+import androidx.lifecycle.ViewModel
+
+class NormalLedgerListViewModel : ViewModel() {
+    // TODO: Implement the ViewModel
+}
